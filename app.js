71fda1313a2e267b0a2e340a0bca60705b9a626a
senoari000 <senoari000@gmail.com>
2022-01-18 14:01:18 +0700

    first init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 8000 || 8080;
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/api", (req, res) => {
+  res.send("welcome to server");
+});
+
+app.listen(PORT, () => {
+  console.log(`app running at PORT: ${PORT}`);
+});
